a70e5517a5795b28a1b00b7c40ca38bcffabb861
yaowen <tanghaihua1212@163.com>
2022-07-28 20:54:35 +0800

    feat bind question data to UI

--- app/src/main/java/com/example/demo001/QuizQuestionActivity.kt
+++ app/src/main/java/com/example/demo001/QuizQuestionActivity.kt
@@ -3,14 +3,56 @@ package com.example.demo001
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.ImageView
+import android.widget.ProgressBar
+import android.widget.TextView
 
 class QuizQuestionActivity : AppCompatActivity() {
+    private var progressBar: ProgressBar? = null
+    private var tvProgress: TextView? = null
+    private var tvQuestion: TextView? = null
+    private var ivImage: ImageView? = null
+    private var tvOptionOne: TextView? = null
+    private var tvOptionTwo: TextView? = null
+    private var tvOptionThree: TextView? = null
+    private var tvOptionFour: TextView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz_question)
 
+        progressBar = findViewById(R.id.progressBar)
+        tvProgress = findViewById(R.id.tv_progress)
+
+        ivImage = findViewById(R.id.iv_image)
+
+        tvQuestion = findViewById(R.id.tv_question)
+        tvOptionOne = findViewById(R.id.tv_option_one)
+        tvOptionTwo = findViewById(R.id.tv_option_two)
+        tvOptionThree = findViewById(R.id.tv_option_three)
+        tvOptionFour = findViewById(R.id.tv_option_four)
+
+
         val questionsList = Constants.getQuestions()
 
         Log.i("QuestionsLIst size is ", "${questionsList.size}")
+
+        for(i in questionsList) {
+            Log.e("Questions", i.question)
+        }
+
+        var currentPosition = 0
+        val question: Question = questionsList[currentPosition]
+        progressBar?.progress = currentPosition + 1
+        tvProgress?.text = "${progressBar?.progress}/${progressBar?.max}"
+        tvQuestion?.text = question.question
+
+        ivImage?.setImageResource(question.image)
+        tvOptionOne?.text = question.optionOne
+        tvOptionTwo?.text = question.optionTwo
+        tvOptionThree?.text = question.optionThree
+        tvOptionFour?.text = question.optionFour
+
+
     }
 }
